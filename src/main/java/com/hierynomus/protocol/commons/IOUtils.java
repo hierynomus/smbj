d2a6fc61935aedbcd5887375f9f56efda1b727ed
Jeroen van Erp <jeroen@hierynomus.com>
2016-01-25 14:22:01 +0100

    Added Apache2 license header

--- src/main/java/com/hierynomus/protocol/commons/IOUtils.java
+++ src/main/java/com/hierynomus/protocol/commons/IOUtils.java
@@ -1,5 +1,5 @@
-/**
- * Copyright 2009 sshj contributors
+/*
+ * Copyright (C)2016 - Jeroen van Erp <jeroen@hierynomus.com>
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
