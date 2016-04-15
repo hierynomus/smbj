a85ce6621c6e42e68c2afc13d4838cf764c6629e
Jeroen van Erp <jeroen@hierynomus.com>
2016-04-15 10:17:30 +0200

    Updated license header to point to contributors

--- src/main/java/com/hierynomus/spnego/SpnegoException.java
+++ src/main/java/com/hierynomus/spnego/SpnegoException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C)2016 - Jeroen van Erp <jeroen@hierynomus.com>
+ * Copyright (C)2016 - SMBJ Contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
