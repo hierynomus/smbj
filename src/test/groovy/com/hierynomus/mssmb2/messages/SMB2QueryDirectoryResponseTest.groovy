973a86db72729c48ddf21c78aac2d1c78ad3f71b
Jeroen van Erp <jeroen@hierynomus.com>
2016-11-15 12:20:01 +0100

    Moved tests to correct package

--- src/test/groovy/com/hierynomus/mssmb2/messages/SMB2QueryDirectoryResponseTest.groovy
+++ src/test/groovy/com/hierynomus/mssmb2/messages/SMB2QueryDirectoryResponseTest.groovy
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.hierynomus.mssmb2
+package com.hierynomus.mssmb2.messages
 
 import com.hierynomus.msfscc.FileInformationClass
 import com.hierynomus.msfscc.fileinformation.FileInformationFactory
