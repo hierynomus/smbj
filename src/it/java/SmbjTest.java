63b840c664923e34da29ff8083d1e9c466e2f797
Saravanan Bellan <sbellan@gmail.com>
2016-11-29 00:29:23 -0800

    - SID equals/hashCode - More NtStatus defined - Add the actual NtStatus in the api exception - Directory list to consume the full output

--- src/it/java/SmbjTest.java
+++ src/it/java/SmbjTest.java
@@ -333,6 +333,15 @@ public class SmbjTest {
     }
 
 
+    @Test
+    public void testSID() {
+        SID s1 = new SID((byte) 1, new byte[]{0, 0, 0, 0, 0, 1}, new long[]{0});
+        SID s2 = new SID((byte) 1, new byte[]{0, 0, 0, 0, 0, 1}, new long[]{0});
+        assertEquals(s1, s2);
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
+    }
+
     //@Test
     public void manualTestNotify() throws IOException {
 
