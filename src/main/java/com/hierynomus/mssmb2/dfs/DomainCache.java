5d7dacf3c70e5313fb5e12fd9f97fad8f6ee0a2b
ckherrmann <ckherrmann@gmail.com>
2016-11-30 09:36:42 -0800

    Javadoc and formatting fixes

--- src/main/java/com/hierynomus/mssmb2/dfs/DomainCache.java
+++ src/main/java/com/hierynomus/mssmb2/dfs/DomainCache.java
@@ -15,46 +15,47 @@
  */
 package com.hierynomus.mssmb2.dfs;
 
-import static com.hierynomus.protocol.commons.EnumWithValue.EnumUtils.isSet;
-
-import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
-import com.hierynomus.mssmb2.dfs.ReferralCache.RootOrLink;
-import com.hierynomus.mssmb2.dfs.ReferralCache.TargetSetEntry;
-import com.hierynomus.mssmb2.dfs.SMB2GetDFSReferralResponse.ReferralHeaderFlags;
-
-// [MS-DFSC]:
-// DomainCache: Applicable only for a computer joined to a domain. This cache contains a list of trusted domains in 
-// both NetBIOS and fully qualified domain name forms, in addition to a list of DC host names for each domain. 
-// Conceptually, this is an array of tuples of the form <DomainName, DCHint, DCList>. Cache lookup involves finding 
-// a DomainCache entry with a matching DomainName. This can be used to check for a valid domain name or to find a DC 
-// host name for a given domain name. DCHint identifies a DC host name from DCList that is the DC that was last 
-// successfully used by the DFS client.
+/**
+ * [MS-DFSC].pdf 3.1.1 Abstract Data Model DomainCache: Applicable only for a
+ * computer joined to a domain. This cache contains a list of trusted domains in
+ * both NetBIOS and fully qualified domain name forms, in addition to a list of
+ * DC host names for each domain. Conceptually, this is an array of tuples of
+ * the form <DomainName, DCHint, DCList>. Cache lookup involves finding a
+ * DomainCache entry with a matching DomainName. This can be used to check for a
+ * valid domain name or to find a DC host name for a given domain name. DCHint
+ * identifies a DC host name from DCList that is the DC that was last
+ * successfully used by the DFS client.
+ */
 public class DomainCache {
-    private Hashtable<String,DomainCacheEntry> cache = new Hashtable<String,DomainCacheEntry>(); //TODO reasonable initial size
-    
+    private Hashtable<String, DomainCacheEntry> cache = new Hashtable<String, DomainCacheEntry>();
+
+    // TODO reasonable initial size
+
     public class DomainCacheEntry {
         String domainName;
         String DCHint;
         List<String> DCList;
-//        3.1.5.4.2 Receiving a DC Referral Response
-//        This is applicable only to a domain-joined computer. The DFS client receives this referral response for the DC 
-//        referral request that it sent in step 5.2 of section 3.1.4.1. The DC referral response MUST be version 3 or later; 
-//        otherwise, the client MUST ignore the referral response.
-//        The client MUST verify that the NumberOfReferrals field of the referral header is 1 and that the NameListReferral 
-//        bit is set in the referral entry. The other bits of ReferralEntryFlags in the referral entry MUST be ignored. 
-//        The NumberOfExpandedNames in the referral entry contains the number of DC names returned. The client MUST 
-//        use the value in the NumberOfExpandedNames field to determine how many names are present in the list at 
-//        ExpandedNameOffset. The client can access the first null-terminated Unicode DC name string that is returned by 
-//        adding the value in the ExpandedNameOffset field to the address of the referral entry. Immediately following the 
-//        null termination of a DC name is the next DC name returned. The client can access the null-terminated Unicode 
-//        domain name that corresponds to the referral response by adding the value in the SpecialNameOffset to the 
-//        address of the referral entry.
-//        The client MUST add the list of DCs determined for a domain name to DCList of the DomainCache entry that 
-//        corresponds to the domain name. If the DomainCache entry's DCList is not empty, the client MUST replace 
-//        it with the DC list from the referral response and set DCHint to the first DC in the new DCList.        
+
+        //      3.1.5.4.2 Receiving a DC Referral Response
+        //      This is applicable only to a domain-joined computer. The DFS client receives this referral response for the DC
+        //      referral request that it sent in step 5.2 of section 3.1.4.1. The DC referral response MUST be version 3 or later;
+
+        //      otherwise, the client MUST ignore the referral response.
+        //      The client MUST verify that the NumberOfReferrals field of the referral header is 1 and that the NameListReferral
+        //      bit is set in the referral entry. The other bits of ReferralEntryFlags in the referral entry MUST be ignored.
+        //      The NumberOfExpandedNames in the referral entry contains the number of DC names returned. The client MUST
+        //      use the value in the NumberOfExpandedNames field to determine how many names are present in the list at
+        //      ExpandedNameOffset. The client can access the first null-terminated Unicode DC name string that is returned by
+        //      adding the value in the ExpandedNameOffset field to the address of the referral entry. Immediately following the
+        //      null termination of a DC name is the next DC name returned. The client can access the null-terminated Unicode
+        //      domain name that corresponds to the referral response by adding the value in the SpecialNameOffset to the
+        //      address of the referral entry.
+        //      The client MUST add the list of DCs determined for a domain name to DCList of the DomainCache entry that
+        //      corresponds to the domain name. If the DomainCache entry's DCList is not empty, the client MUST replace
+        //      it with the DC list from the referral response and set DCHint to the first DC in the new DCList.
         public DomainCacheEntry(SMB2GetDFSReferralResponse response) {
             List<DFSReferral> l = response.referralEntries;
             DFSReferral rr = response.referralEntries.get(0);
@@ -70,6 +71,7 @@ public class DomainCache {
     public DomainCacheEntry lookup(String domainName) {
         return cache.get(domainName);
     }
+
     public void put(DomainCacheEntry domainCacheEntry) {
         cache.put(domainCacheEntry.domainName, domainCacheEntry);
     }
--- src/main/java/com/hierynomus/mssmb2/dfs/ReferralCache.java
+++ src/main/java/com/hierynomus/mssmb2/dfs/ReferralCache.java
@@ -15,57 +15,58 @@
  */
 package com.hierynomus.mssmb2.dfs;
 
+import static com.hierynomus.protocol.commons.EnumWithValue.EnumUtils.isSet;
+
 import java.util.ArrayList;
 import java.util.Hashtable;
 import java.util.List;
 
-import static com.hierynomus.protocol.commons.EnumWithValue.EnumUtils.isSet;
-import static com.hierynomus.mssmb2.dfs.SMB2GetDFSReferralResponse.ReferralHeaderFlags;
+import com.hierynomus.mssmb2.dfs.SMB2GetDFSReferralResponse.ReferralHeaderFlags;
 
+/**
+ * [MS-DFSC].pdf: 3.1.1 Abstract Data Model
+ * <p>
+ * ReferralCache: This cache contains root, link, and sysvol referral responses.
+ * A hit on a ReferralCache entry indicates that the path in a name resolution
+ * operation is a DFS Root, DFS link, or a SYSVOL/NETLOGON share. A
+ * ReferralCache entry conceptually contains entries indexed by a DFS path
+ * prefix, DFSPathPrefix. An entry is a tuple of the form <DFSPathPrefix,
+ * RootOrLink, Interlink, TTL, TargetFailback, TargetHint, TargetList>.
+ * DFSPathPrefix is the DFS path that corresponds to a DFS root or a DFS link,
+ * and is the same as the string pointed to by the DFSPathOffset of a
+ * DFS_REFERRAL_V2, DFS_REFERRAL_V3 or DFS_REFERRAL_V4 referral entry.
+ * RootOrLink identifies whether the entry contains DFS root targets or DFS link
+ * targets. It reflects the value from the ServerType field of a referral entry
+ * (as specified in sections 2.2.5.1, 2.2.5.2, 2.2.5.3, and 2.2.5.4). Interlink
+ * identifies whether the entry contains a target in another DFS namespace, as
+ * determined by the test in section 3.1.5.4.5. TargetFailback is used only for
+ * DFS_REFERRAL_V4 and contains the value from the TargetFailback bit of the
+ * referral header (as specified in section 2.2.4). TTL contains a value derived
+ * from the TimeToLive field of a referral entry (as specified in sections
+ * 2.2.5.1, 2.2.5.2, 2.2.5.3, and 2.2.5.4). This is the time stamp at which a
+ * ReferralCache entry is considered to be expired. An implementation is free to
+ * come up with soft and hard time-outs based on the TimeToLive field of the
+ * referral entry, for example. The soft time-out can be used to initiate a
+ * ReferralCache entry refresh operation while permitting the use of the
+ * ReferralCache entry; the hard time-out limit can be used to fail any
+ * operation using the ReferralCache entry if all attempts to refresh it
+ * fail.<4> TargetHint identifies a target in TargetList that was last
+ * successfully used by the DFS client. TargetList consists of tuples of the
+ * form <TargetPath, TargetSetBoundary>, where TargetPath is the string pointed
+ * to by the NetworkAddressOffset field (as specified in sections 2.2.5.2,
+ * 2.2.5.3, and 2.2.5.4). TargetSetBoundary is only present in V4 referrals and
+ * reflects the value from the TargetSetBoundary of the referral entry (as
+ * specified in section 2.2.5.4).
+ */
 public class ReferralCache {
-    // [MS-DFSC]:
-    // ReferralCache: This cache contains root, link, and sysvol referral
-    // responses. A hit on a ReferralCache entry indicates that the path in a
-    // name resolution operation is a DFS Root, DFS link, or a SYSVOL/NETLOGON
-    // share.
-    // A ReferralCache entry conceptually contains entries indexed by a DFS path
-    // prefix, DFSPathPrefix. An entry is a tuple of the form <DFSPathPrefix,
-    // RootOrLink, Interlink, TTL, TargetFailback, TargetHint, TargetList>.
-    // DFSPathPrefix is the DFS path that corresponds to a DFS root or a DFS
-    // link, and is the same as the string pointed to by the DFSPathOffset of a
-    // DFS_REFERRAL_V2, DFS_REFERRAL_V3 or DFS_REFERRAL_V4 referral entry.
-    // RootOrLink identifies whether the entry contains DFS root targets or DFS
-    // link targets. It reflects the value from the ServerType field of a
-    // referral entry (as specified in sections 2.2.5.1, 2.2.5.2, 2.2.5.3, and
-    // 2.2.5.4).
-    // Interlink identifies whether the entry contains a target in another DFS
-    // namespace, as determined by the test in section 3.1.5.4.5.
-    // TargetFailback is used only for DFS_REFERRAL_V4 and contains the value
-    // from the TargetFailback bit of the referral header (as specified in
-    // section 2.2.4).
-    // TTL contains a value derived from the TimeToLive field of a referral
-    // entry (as specified in sections 2.2.5.1, 2.2.5.2, 2.2.5.3, and 2.2.5.4).
-    // This is the time stamp at which a ReferralCache entry is considered to be
-    // expired. An implementation is free to come up with soft and hard
-    // time-outs based on the TimeToLive field of the referral entry, for
-    // example. The soft time-out can be used to initiate a ReferralCache entry
-    // refresh operation while permitting the use of the ReferralCache entry;
-    // the hard time-out limit can be used to fail any operation using the
-    // ReferralCache entry if all attempts to refresh it fail.<4>
-    // TargetHint identifies a target in TargetList that was last successfully
-    // used by the DFS client. TargetList consists of tuples of the form
-    // <TargetPath, TargetSetBoundary>, where TargetPath is the string pointed
-    // to by the NetworkAddressOffset field (as specified in sections 2.2.5.2,
-    // 2.2.5.3, and 2.2.5.4). TargetSetBoundary is only present in V4 referrals
-    // and reflects the value from the TargetSetBoundary of the referral entry
-    // (as specified in section 2.2.5.4).
 
     Hashtable<String, ReferralCacheEntry> cache = new Hashtable<String, ReferralCacheEntry>();
 
     // TODO what's a reasonable initial capacity? Large?
 
     public enum RootOrLink {
-        RCE_LINK(0x0), RCE_ROOT(0x1);
+        RCE_LINK(0x0),
+        RCE_ROOT(0x1);
         RootOrLink(int value) {
             this.value = value;
         }
@@ -100,7 +101,7 @@ public class ReferralCache {
             this.dfsPathPrefix = response.referralEntries.get(0).dfsPath;
             this.rootOrLink = RootOrLink.get(response.referralEntries.get(0).serverType);
             this.interlink = isSet(response.referralHeaderFlags, ReferralHeaderFlags.ReferralServers)
-                            && !isSet(response.referralHeaderFlags, ReferralHeaderFlags.StorageServers);
+                    && !isSet(response.referralHeaderFlags, ReferralHeaderFlags.StorageServers);
             // TODO this is ugly
             if (!this.interlink) {
                 if (response.referralEntries.size() == 1) {
@@ -111,7 +112,7 @@ public class ReferralCache {
             this.ttl = response.referralEntries.get(0).timeToLive;
             this.expires = System.currentTimeMillis() + this.ttl * 1000L;
             this.targetFailback = isSet(response.referralHeaderFlags,
-                            SMB2GetDFSReferralResponse.ReferralHeaderFlags.TargetFailback);
+                    SMB2GetDFSReferralResponse.ReferralHeaderFlags.TargetFailback);
             List<TargetSetEntry> targetList = new ArrayList<TargetSetEntry>(response.referralEntries.size());
             for (DFSReferral r : response.referralEntries) {
                 TargetSetEntry e = new TargetSetEntry();
