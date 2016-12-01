fa624df3de518f1a6d04858b475386ebf2b82225
Jeroen van Erp <jeroen@hierynomus.com>
2016-12-01 09:21:01 +0100

    Improved SID TEst

--- src/test/groovy/com/hierynomus/msdtyp/SIDTest.groovy
+++ src/test/groovy/com/hierynomus/msdtyp/SIDTest.groovy
@@ -25,8 +25,12 @@ class SIDTest extends Specification {
     }
 
     def "SID identity"() {
+      given:
       SID s1 = new SID((byte) 1, [0, 0, 0, 0, 0, 1] as byte[], [0] as long[]);
       SID s2 = new SID((byte) 1, [0, 0, 0, 0, 0, 1] as byte[], [0] as long[]);
+
+      expect:
       s1 == s2
+      s1.hashCode() == s2.hashCode()
     }
 }
