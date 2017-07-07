10bb4821baaf83035c905c3d4aefb05a72c17035
Jeroen van Erp <jeroen@hierynomus.com>
2017-07-07 13:54:50 +0200

    Stale connections are removed from SMBClient (Fixes #114) (#115)

--- src/main/java/com/hierynomus/smbj/SMBClient.java
+++ src/main/java/com/hierynomus/smbj/SMBClient.java
@@ -16,7 +16,11 @@
 package com.hierynomus.smbj;
 
 import com.hierynomus.smbj.connection.Connection;
+import com.hierynomus.smbj.event.ConnectionClosed;
 import com.hierynomus.smbj.event.SMBEventBus;
+import net.engio.mbassy.listener.Handler;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.util.Map;
@@ -44,6 +48,7 @@ public class SMBClient {
     public SMBClient(SmbConfig config) {
         this.config = config;
         bus = new SMBEventBus();
+        bus.subscribe(this);
     }
 
     /**
@@ -71,13 +76,26 @@ public class SMBClient {
 
     private Connection getEstablishedOrConnect(String hostname, int port) throws IOException {
         synchronized (this) {
-            if (!connectionTable.containsKey(hostname)) {
+            String hostPort = hostname + ":" + port;
+            if (!connectionTable.containsKey(hostPort)) {
                 Connection connection = new Connection(config, bus);
                 connection.connect(hostname, port);
-                connectionTable.put(hostname, connection);
+                connectionTable.put(hostPort, connection);
                 return connection;
             }
-            return connectionTable.get(hostname);
+            return connectionTable.get(hostPort);
         }
     }
+
+    @Handler
+    @SuppressWarnings("unused")
+    private void connectionClosed(ConnectionClosed event) {
+        synchronized (this) {
+            String hostPort = event.getHostname() + ":" + event.getPort();
+            connectionTable.remove(hostPort);
+            log.debug("Connection to << {} >> closed", hostPort);
+        }
+    }
+
+    private static final Logger log = LoggerFactory.getLogger(SMBClient.class);
 }
