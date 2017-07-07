27c507575fa45c65a4a60ccc869e96d62757ae83
Oran Epelbaum <oran@epelbaum.me>
2017-07-07 10:25:00 +0300

    Async TCP transport (#111)
    * First shot at doing an async transport
    
    * Added some needed calls to flip()
    
    * Moved CrossThreadLock to package, wrote test and fixed hang bug.
    
    * Async transport fixes + comments
    
    * Added convenience method for a super-simple async channel group configuration.
    
    * Changed default test time to 2s per test (can provide a large value via system properties for stress-testing).
    
    * Fixed some codacy issues
    
    * Changed concurrent write handling, using queue with simple synchronization isntead of CrossThreadLock.y
    
    * Fixed state sync applying only successful write.
    
    * Removed use of JDK8-only constant.
    
    * Changed factory constructors and removed built-in single thread option
    
    * Chained constructors
    
    * Moved packet bytes read logic to separate PacketBufferReader class.
    
    * Added copyright header
    
    * More consistent use of buffer reader in completion handler.
    
    * Redundant import

---
+++ src/main/java/com/hierynomus/smbj/connection/AsyncDirectTcpTransportFactory.java
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C)2016 - SMBJ Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hierynomus.smbj.connection;
+
+import java.io.IOException;
+import java.nio.channels.AsynchronousChannelGroup;
+import java.util.concurrent.ExecutorService;
+
+import com.hierynomus.mssmb2.SMB2Packet;
+import com.hierynomus.mssmb2.messages.SMB2MessageConverter;
+import com.hierynomus.smbj.SmbConfig;
+import com.hierynomus.smbj.common.SMBRuntimeException;
+import com.hierynomus.smbj.transport.PacketHandlers;
+import com.hierynomus.smbj.transport.PacketReceiver;
+import com.hierynomus.smbj.transport.TransportLayer;
+import com.hierynomus.smbj.transport.tcp.AsyncDirectTcpTransport;
+
+public class AsyncDirectTcpTransportFactory implements TransportLayerFactory<SMB2Packet> {
+    private static final AsynchronousChannelGroup DEFAULT_CHANNEL_GROUP = null;  // use system default
+    private static final SMB2MessageConverter converter = new SMB2MessageConverter();
+    private final AsynchronousChannelGroup group;
+
+    @Override
+    public TransportLayer<SMB2Packet> createTransportLayer(PacketReceiver<SMB2Packet> receiver, SmbConfig config) {
+        PacketHandlers<SMB2Packet> handlers = new PacketHandlers<>(converter, receiver, converter);
+        try {
+            return new AsyncDirectTcpTransport<SMB2Packet>(config.getSoTimeout(), handlers, group);
+        } catch (IOException e) {
+            throw new SMBRuntimeException(e);
+        }
+    }
+
+    public AsyncDirectTcpTransportFactory() {
+        this((AsynchronousChannelGroup) DEFAULT_CHANNEL_GROUP);
+    }
+
+    public AsyncDirectTcpTransportFactory(ExecutorService executor) {
+        this(createGroup(executor));
+    }
+
+    public AsyncDirectTcpTransportFactory(AsynchronousChannelGroup group) {
+        this.group = group;
+    }
+
+    private static AsynchronousChannelGroup createGroup(ExecutorService executor) {
+        try {
+            return AsynchronousChannelGroup.withThreadPool(executor);
+        } catch (IOException e) {
+            throw new RuntimeException(e); // unable to create new threads?
+        }
+    }
+}
---
+++ src/main/java/com/hierynomus/smbj/transport/tcp/AsyncPacketReader.java
@@ -0,0 +1,142 @@
+/*
+ * Copyright (C)2016 - SMBJ Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.hierynomus.smbj.transport.tcp;
+
+import java.io.EOFException;
+import java.io.IOException;
+import java.nio.channels.AsynchronousCloseException;
+import java.nio.channels.AsynchronousSocketChannel;
+import java.nio.channels.CompletionHandler;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.hierynomus.protocol.Packet;
+import com.hierynomus.protocol.commons.buffer.Buffer.BufferException;
+import com.hierynomus.smbj.transport.PacketFactory;
+import com.hierynomus.smbj.transport.PacketReader;
+import com.hierynomus.smbj.transport.PacketReceiver;
+import com.hierynomus.smbj.transport.TransportException;
+
+public class AsyncPacketReader<P extends Packet<P, ?>> {
+    private static final Logger logger = LoggerFactory.getLogger(PacketReader.class);
+
+    private final PacketFactory<P> packetFactory;
+    private PacketReceiver<P> handler;
+    private final AsynchronousSocketChannel channel;
+    private String remoteHost;
+    private int soTimeout = 0;
+
+    private AtomicBoolean stopped = new AtomicBoolean(false);
+
+    public AsyncPacketReader(AsynchronousSocketChannel channel, PacketFactory<P> packetFactory,
+            PacketReceiver<P> handler) {
+        this.channel = channel;
+        this.packetFactory = packetFactory;
+        this.handler = handler;
+    }
+
+    public void start(String remoteHost, int soTimeout) {
+        this.remoteHost = remoteHost;
+        this.soTimeout = soTimeout;
+        initiateNextRead(new PacketBufferReader());
+    }
+
+    public void stop() {
+        stopped.set(true);
+    }
+
+    private void initiateNextRead(PacketBufferReader bufferReader) {
+        if (stopped.get()) {
+            logger.trace("Stopped, not initiating another read operation.");
+            return;
+        }
+        logger.trace("Initiating next read");
+        channel.read(bufferReader.getBuffer(), this.soTimeout, TimeUnit.MILLISECONDS, bufferReader,
+                new CompletionHandler<Integer, PacketBufferReader>() {
+
+                    @Override
+                    public void completed(Integer bytesRead, PacketBufferReader reader) {
+                        logger.trace("Received {} bytes", bytesRead);
+                        if (bytesRead < 0) {
+                            handleClosedReader();
+                            return; // stop the read cycle
+                        }
+                        try {
+                            processPackets(reader);
+                            initiateNextRead(reader);
+                        } catch (RuntimeException e) {
+                            handleAsyncFailure(e);
+                        }
+                    }
+
+                    @Override
+                    public void failed(Throwable exc, PacketBufferReader attachment) {
+                        handleAsyncFailure(exc);
+                    }
+
+                    private void processPackets(PacketBufferReader reader) {
+                        for (byte[] packetBytes = reader.readNext(); packetBytes != null; packetBytes = reader
+                                .readNext()) {
+                            readAndHandlePacket(packetBytes);
+                        }
+                    }
+
+                    private void handleClosedReader() {
+                        if (!stopped.get()) {
+                            handleAsyncFailure(new EOFException("Connection closed by server"));
+                        }
+                    }
+
+                });
+    }
+
+    private void readAndHandlePacket(byte[] packetBytes) {
+        try {
+            P packet = packetFactory.read(packetBytes);
+            logger.trace("Received packet << {} >>", packet);
+            handler.handle(packet);
+        } catch (BufferException | TransportException e) {
+            handleAsyncFailure(e);
+        }
+    }
+
+    private void handleAsyncFailure(Throwable exc) {
+        if (isChannelClosedByOtherParty(exc)) {
+            logger.trace("Channel to {} closed by other party, closing it locally.", remoteHost);
+        } else {
+            String excClass = exc.getClass().getSimpleName();
+            logger.trace("{} on channel to {}, closing channel: {}", excClass, remoteHost, exc.getMessage());
+        }
+        closeChannelQuietly();
+    }
+
+    private boolean isChannelClosedByOtherParty(Throwable exc) {
+        return exc instanceof AsynchronousCloseException;
+    }
+
+    private void closeChannelQuietly() {
+        try {
+            channel.close();
+        } catch (IOException e) {
+            String eClass = e.getClass().getSimpleName();
+            logger.debug("{} while closing channel to {} on failure: {}", eClass, remoteHost, e.getMessage());
+        }
+    }
+
+}
