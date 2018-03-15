f5fc954adef3af9ebcc6ca2b332a378aeff1558e
Pepijn Van Eeckhoudt <pepijn@vaneeckhoudt.net>
2018-03-15 15:32:34 +0100

    Simplify remote copy implementation

--- src/main/java/com/hierynomus/mssmb2/copy/CopyChunkRequest.java
+++ src/main/java/com/hierynomus/mssmb2/copy/CopyChunkRequest.java
@@ -1,4 +1,21 @@
+/*
+ * Copyright (C)2016 - SMBJ Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hierynomus.mssmb2.copy;
+
+import com.hierynomus.smb.SMBBuffer;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,8 +29,9 @@ public class CopyChunkRequest {
     private byte[] resumeKey;
     private List<Chunk> chunks = new ArrayList<>();
 
-    public CopyChunkRequest(byte[] resumeKey) {
+    public CopyChunkRequest(byte[] resumeKey, List<Chunk> chunks) {
         this.resumeKey = resumeKey;
+        this.chunks.addAll(chunks);
     }
 
     public static long getCtlCode() {
@@ -26,6 +44,18 @@ public class CopyChunkRequest {
 
     public List<Chunk> getChunks() {
         return chunks;
+    }
+
+    public void write(SMBBuffer buffer) {
+        buffer.putRawBytes(getResumeKey());  //RESUME KEY
+        buffer.putUInt32(getChunks().size());    //CHUNK COUNT
+        buffer.putUInt32(0l);    //reserved
+        for (CopyChunkRequest.Chunk chunk : getChunks()){
+            buffer.putUInt64(chunk.getSrcOffset());//source offset
+            buffer.putUInt64(chunk.getTgtOffset());//target offset
+            buffer.putUInt32(chunk.getLength()); //length
+            buffer.putUInt32(0); //reserved 0 always
+        }
     }
 
     /**
--- src/main/java/com/hierynomus/mssmb2/copy/CopyChunkResponse.java
+++ src/main/java/com/hierynomus/mssmb2/copy/CopyChunkResponse.java
@@ -1,12 +1,33 @@
+/*
+ * Copyright (C)2016 - SMBJ Contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.hierynomus.mssmb2.copy;
 
+import com.hierynomus.protocol.commons.buffer.Buffer;
+import com.hierynomus.smb.SMBBuffer;
+
 /**
- *https://msdn.microsoft.com/en-us/library/cc246549.aspx
+ * https://msdn.microsoft.com/en-us/library/cc246549.aspx
  */
 public class CopyChunkResponse {
     private long chunksWritten;
     private long chunkBytesWritten;
     private long totalBytesWritten;
+
+    public CopyChunkResponse() {
+    }
 
     public CopyChunkResponse(long chunksWritten, long chunkBytesWritten, long totalBytesWritten) {
         this.chunksWritten = chunksWritten;
@@ -24,5 +45,11 @@ public class CopyChunkResponse {
 
     public long getTotalBytesWritten() {
         return totalBytesWritten;
+    }
+
+    public final void read(SMBBuffer in) throws Buffer.BufferException {
+        chunksWritten = in.readUInt32();
+        chunkBytesWritten = in.readUInt32();
+        totalBytesWritten = in.readUInt32();
     }
 }
