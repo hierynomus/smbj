69439cd7ba47170f10a40c69044a80b5bdaa6f61
yin19941005 <yin19941005@hotmail.com>
2018-04-26 15:50:13 +0800

    Add maximalAccess to TreeConnect and change in treeConnectResponse (#319)
    Add maximalAccess to TreeConnect class. Change the getMaximalAccess() in SMB2TreeConnectResponse to Set<AccessMask> instead of long. Change the test case to use Set<AccessMask> to compare instead of using long.

--- src/main/java/com/hierynomus/smbj/share/TreeConnect.java
+++ src/main/java/com/hierynomus/smbj/share/TreeConnect.java
@@ -15,6 +15,7 @@
  */
 package com.hierynomus.smbj.share;
 
+import com.hierynomus.msdtyp.AccessMask;
 import com.hierynomus.mssmb2.SMB2Packet;
 import com.hierynomus.mssmb2.SMB2ShareCapabilities;
 import com.hierynomus.mssmb2.messages.SMB2TreeDisconnect;
@@ -42,14 +43,16 @@ public class TreeConnect {
     private final Set<SMB2ShareCapabilities> capabilities;
     private Connection connection;
     private final SMBEventBus bus;
+    private final Set<AccessMask> maximalAccess;
 
-    public TreeConnect(long treeId, SmbPath smbPath, Session session, Set<SMB2ShareCapabilities> capabilities, Connection connection, SMBEventBus bus) {
+    public TreeConnect(long treeId, SmbPath smbPath, Session session, Set<SMB2ShareCapabilities> capabilities, Connection connection, SMBEventBus bus, Set<AccessMask> maximalAccess) {
         this.treeId = treeId;
         this.smbPath = smbPath;
         this.session = session;
         this.capabilities = capabilities;
         this.connection = connection;
         this.bus = bus;
+        this.maximalAccess = maximalAccess;
     }
 
     Connection getConnection() {
@@ -79,6 +82,10 @@ public class TreeConnect {
 
     public Session getSession() {
         return session;
+    }
+
+    public Set<AccessMask> getMaximalAccess() {
+        return maximalAccess;
     }
 
     public boolean isDfsShare() {
